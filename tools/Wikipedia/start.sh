2b6affc1936490a5efe7cdc5107b12bc70092900
Devis Lucato <dluc@users.noreply.github.com>
2025-04-15 18:51:59 -0700

    Fix python image port

--- tools/Wikipedia/start.sh
+++ tools/Wikipedia/start.sh
@@ -5,8 +5,8 @@ set -e
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]:-$0}")" && pwd)"
 cd "$HERE"
 
-PORT=${PORT:-5021}
+UVICORN_PORT=${UVICORN_PORT:-5021}
 
 poetry install
 
-poetry run uvicorn app.main:app --reload --port "$PORT"
+poetry run uvicorn app.main:app --reload --port "$UVICORN_PORT"
